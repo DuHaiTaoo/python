3a4f1dc73d5ad992a2ee64157922f3fd7de670ce
cold-mo <1362073193@qq.com>
2018-05-26 01:37:35 +0800

    Add content

--- dht.py
+++ dht.py
@@ -1,27 +1,56 @@
 #!/usr/bin/env python3
 #-*-coding:utf-8;-*-
 
+'''
+    By:cold-mo(cm)
+'''
 
 import xlrd
 import urllib.request
 import re
 import sys
 import os
+import hashlib
+import hmac
 
 
+def isEmail(data):
+    '''
+        判断字符串或列表内容是否是邮箱地址
+    '''
+    if isinstance(data,list):
+        a = {}
+        for i in data: 
+            if str(re.match(r'([\.a-z0-9A-Z]+?@[0-9a-zA-Z]+?\.[0-9a-zA-Z]+)',str(i))) != 'None':
+                a[i] = True
+            else:
+                a[i] = False
+        return a
+    if isinstance(data,str):
+        if str(re.match(r'([\.a-z0-9A-Z]+?@[0-9a-zA-Z]+?\.[0-9a-zA-Z]+)',str(data))) != 'None':
+            return True
+        else:
+            return False
 
-def dht():
-    print('this is cold-mo first use github!')
 
-def dhtExhaustionFolder(Folder):
-    for i in os.walk(Folder):
-        for n in i[2]:
-            print (n)
+def calcMd5(s):
+    '''
+        返回字符串的md5值
+    '''
+    return hashlib.md5().update(s.encode('utf-8')).hexdigest()
+
+def calcSha1(s):
+    '''
+        返回字符串的sha1值
+    '''
+    return = hashlib.sha1().update(s.encode('utf-8')).hexdigest()
+
+def hmac_md5(key, s):
+    return hmac.new(key.encode('utf-8'), s.encode('utf-8'), 'MD5').hexdigest()
 
 
-    #[n.split('.')[0] for i in os.walk(path) for n in i[2]]
 
 
 if __name__ == '__main__':
-    #dht()
-    dhtExhaustionFolder('D:\\python\\')
+    
+    pass
---
+++ test.py
@@ -0,0 +1,22 @@
+
+from urllib import request
+import json
+
+def fetch_data(url):
+    with request.urlopen(url) as f:
+        data = f.read()
+        a = json.loads(data)
+        
+
+    return(a)
+
+
+if __name__ == '__main__':
+    URL = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20%3D%202151330&format=json'
+    data = fetch_data(URL)
+    #print(data)
+    assert data['query']['results']['channel']['location']['city'] == 'Beijing'
+    print('ok')
+
+
+    pass
